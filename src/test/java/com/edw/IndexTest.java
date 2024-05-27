f51c8ffd02e36a12af19a897c98a4c0b7b2eed33
edwin <edwinkun@gmail.com>
2024-05-27 11:45:13 +0700

    covering test case for main and model :bug: :carrot:

--- src/test/java/com/edw/IndexTest.java
+++ src/test/java/com/edw/IndexTest.java
@@ -1,6 +1,7 @@
 package com.edw;
 
 
+import com.edw.model.Employee;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -27,5 +28,24 @@ public class IndexTest {
     @DisplayName("01. Test Hello World")
     public void helloWorld() {
         Assertions.assertTrue(Boolean.TRUE);
+    }
+
+    @Test
+    @DisplayName("02. Test Main class")
+    public void applicationContextTest() {
+        Main.main(new String[] {});
+    }
+
+    @Test
+    @DisplayName("03. Test Model")
+    public void employeeModel_test() {
+        Employee employee = new Employee(0, "dodol");
+        Assertions.assertEquals(employee.getEmployeeName(), "dodol");
+        Assertions.assertEquals(employee.getEmployeeId(), 0);
+
+        employee.setEmployeeId(1);
+        employee.setEmployeeName("dodol_number_2");
+        Assertions.assertEquals(employee.getEmployeeName(), "dodol_number_2");
+        Assertions.assertEquals(employee.getEmployeeId(), 1);
     }
 }
